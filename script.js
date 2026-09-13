402cd53ff94d8511dd7b3a80861e09f49fafa6ad
JaviG <86110696+s9code@users.noreply.github.com>
2026-09-13 18:48:35 +0200

    feat: añadir lógica para detectar victoria y empate

--- script.js
+++ script.js
@@ -44,7 +44,20 @@ function crearJugador(nombre, ficha) {
 const GameController = (function () {
     // Creación de jugadores
     const jugador = crearJugador("Javi", "X");
+    // creamos la variable computadora con la factoría de jugadores
     const computadora = crearJugador("Computadora", "O");
+    // Combinaciones ganadoras
+    const combinacionesGanadoras = [
+        [0, 1, 2],
+        [3, 4, 5],
+        [6, 7, 8],
+        [0, 3, 6],
+        [2, 5, 8],
+        [1, 4, 7],
+        [0, 4, 8],
+        [2, 4, 6]
+    ];
+
     // Inicialización del jugador actual
     let jugadorActual = jugador;
 
@@ -54,19 +67,73 @@ const GameController = (function () {
         const JugadaValida = GameBoard.colocarFicha(posicion, jugadorActual.ficha);
         // Si la jugada es válida, cambiar el turno
         if (JugadaValida === true) {
-            if (jugadorActual === jugador) {
-                jugadorActual = computadora;
+            // Primero comprobamos si con esa jugada alguien ha ganado
+            if (comprobarGanador() === true) {
+                console.log("Se acabo la partida");
+
+            
+            } else if (comprobarEmpate() === true){
+                console.log("Empate");
             } else {
-                jugadorActual = jugador;
-            };
+                // Si nadie ha ganado todavía, ENTONCES cambiamos el turno
+                if (jugadorActual === jugador) {
+                    jugadorActual = computadora;
+                } else {
+                    jugadorActual = jugador;
+                };
+            }
+            
         };
         // Retornamos la jugada
         return {
-            JugadaValida
+            JugadaValida,
+
         };
     }
+
+    // Función que comprueba si alguien ha ganado
+    function comprobarGanador() {
+        // Obtenemos el tablero
+        const tablero = GameBoard.getCasillas()
+        // Recorremos las combinaciones ganadoras
+        for (let i = 0; i < combinacionesGanadoras.length; i++) {
+            // Obtenemos las posiciones
+            const pos1 = combinacionesGanadoras[i][0];  
+            const pos2 = combinacionesGanadoras[i][1];
+            const pos3 = combinacionesGanadoras[i][2];
+
+            if (tablero[pos1] === tablero[pos2] && tablero[pos1] === tablero[pos3] && tablero[pos1] !== "") {
+                // Si hay tres fichas iguales en una de las combinaciones ganadoras
+                console.log("Ha ganado el jugador");
+                return true;
+            }
+
+        };
+        // Si no hay tres fichas iguales en ninguna de las combinaciones ganadoras
+        return false;
+    }
+
+    function comprobarEmpate() {
+        const tablero = GameBoard.getCasillas();
+
+        if (tablero.includes("")) {
+            return false;
+        } else {
+            return true;
+        }
+
+    }
+
     // Devolvemos un objeto con los métodos públicos para poder acceder a ellos desde fuera
     return {
-        jugarRonda
+        jugarRonda,
+        comprobarGanador
     };
 })();
+
+GameController.jugarRonda(0); // X en 0
+GameController.jugarRonda(6); // O en 6
+GameController.jugarRonda(1); // X en 1
+GameController.jugarRonda(7); // O en 7
+GameController.jugarRonda(2); // X en 2 -> ¡Debería ganar aquí!
+
